e1fb4fb0668753fb05214ce59a5aeea814ccd87c
Jagan-18 <devOps18@gmail.com>
2025-07-25 11:50:03 +0530

    upload file in repo

---
+++ AWS_Topics/AWS Continuous Integration Demo/simple-python-app/start_container.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+# Pull the Docker image from Docker Hub
+echo
+
+# Run the Docker image as a container
+echo "Starting the Docker container..."
+
+
+
+#docker pull simple-python-app:latest
+#docker run -d -p 5000:5000 simple-python-app "simple-python-app:latest"
+#echo "Docker container started successfully." "Pulling the Docker image..."
+#docker pull simple-python-app:latest
+#echo "Docker image pulled successfully." "Pulling the Docker image..."
+#docker pull simple-python-app:latest
+#echo "Docker image pulled successfully."
+#docker run -d -p 5000:5000 simple-python-app:latest
+#echo "Docker container started successfully."               
---
+++ AWS_Topics/AWS Continuous Integration Demo/simple-python-app/stop_container.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+# Stop the running container (if any)
+echo "Hi" "Stopping the Docker container..."
+
+
+#docker pull simple-python-app:latest || true
+#docker run -d -p 5000:5000 simple-python-app:latest || true
+#echo "Docker container started successfully."
+##echo "Pulling the Docker image..."
+#docker pull simple-python-app:latest || true
+#echo "Docker image pulled successfully."
+#echo "Stopping the Docker container..."
+#docker stop simple-python-app || true
+#echo "Docker container stopped successfully."
